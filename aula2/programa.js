b94af7a4416e2b8723f8f1874850cfe9cf546e27
William Soares <pedro.williamsoares2022@gmail.com>
2025-03-13 09:42:16 -0300

    Temas abordados: variáveis, constantes e tipos de dados

---
+++ aula2/programa.js
@@ -0,0 +1,28 @@
+const x = 10;
+const y = 2.5;
+const nome = "William";
+const sobrenome = "Soares";
+const nao_definido = undefined;
+const falso = false;
+const verdade = true;
+const nulo = null;
+
+let nome_usuario = "Usul";
+console.log(nome_usuario);
+nome_usuario = "Atreides";
+console.log(nome_usuario);
+
+console.log(typeof x);// verificando tipo de const
+console.log(x);
+console.log(y);
+console.log(nome);
+console.log(sobrenome);
+console.log(falso);
+console.log(verdade)
+console.log(nulo)
+
+
+
+
+
+
