32717801da9b608265f8d50123946e71873369e3
William Soares <pedro.williamsoares2022@gmail.com>
2025-03-13 19:17:53 -0300

    Assuntos abordados: funções de ordem maior (foreach, map, filter, reduce)

--- aula2/programa.js
+++ aula2/programa.js
@@ -90,11 +90,27 @@ tarefas.forEach(function (tarefa, indice, minhasTarefas) {
 
 })
 
+const vetorDeTextosDasTarefas = tarefas.map(function (tarefa){
+  return `Tarefa: ${tarefa.id} : ${tarefa.texto}`;
+});
+
+console.log(vetorDeTextosDasTarefas);
 
 
+const tarefa1 = tarefas.filter(function (tarefa){
+  return tarefa.id === 1;
+});
 
+console.log(tarefa1);
 
+const vetor = [1, 2, 3, 4];
+const valorInicial = 0;
 
+const soma = vetor.reduce(function (acumulador, elementoAtual){
+  return acumulador + elementoAtual
+}, valorInicial);
+
+console.log(soma);
 
 
 
