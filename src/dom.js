b69444e8dde01a2248598f577b0108c06e7201b9
William Soares <pedro.williamsoares2022@gmail.com>
2025-03-13 23:26:43 -0300

    Assunto abordado: Manipulação do DOM (.remove(), last e firstElementChild, .children, .textContent, .innerText, .innerHTML, .style)

---
+++ src/dom.js
@@ -0,0 +1,21 @@
+const ul = document.querySelector(".itens")
+console.log(ul);
+
+// Remover um elemento
+// ul.remove();
+
+// Remover o ultimo elemento filho
+// ul.lastElementChild.remove();
+
+// Definir o conteudo de texto de um elemento
+// ul.firstElementChild.textContent = "Olá";
+//ul.children[1].textContent = "Oi";
+//ul.children[1].innerText = "Oi";
+
+// Definir o HTML internode um elemento
+//ul.lastElementChild.innerHTML = "<h1>Olá</h1>";
+
+// Alterar os estilos de um elemento de forma dinâmica
+const botao = document.querySelector(".botao");
+console.log(botao);
+botao.style.background = "red";
